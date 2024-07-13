02a987df28a0be6c69e34bdded8f2ed274d6a385
ShiraChehanovich <92167311+ShiraChehanovich@users.noreply.github.com>
2024-07-13 20:43:47 +0300

    main function

--- src/main/java/com/example/bookservice/BookServiceApplication.java
+++ src/main/java/com/example/bookservice/BookServiceApplication.java
@@ -2,12 +2,26 @@ package com.example.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.bookservice.repository")
+@EntityScan(basePackages = "com.example.bookservice.common.entity")
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void runAfterStartup(){
+
+	}
 }
+
+
+
