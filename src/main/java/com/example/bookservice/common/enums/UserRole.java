4f2785fb0a654802040f8967bb78d12385e0b844
ShiraChehanovich <92167311+ShiraChehanovich@users.noreply.github.com>
2024-07-13 20:44:36 +0300

    Added userRole type

---
+++ src/main/java/com/example/bookservice/common/enums/UserRole.java
@@ -0,0 +1,25 @@
+package com.example.bookservice.common.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+
+//    private final int value;
+//
+//    UserRole(int value) {
+//        this.value = value;
+//    }
+//
+//    public int getValue() {
+//        return value;
+//    }
+//
+//    public static UserRole fromValue(int value) {
+//        for (UserRole role : values()) {
+//            if (role.getValue() == value) {
+//                return role;
+//            }
+//        }
+//        throw new IllegalArgumentException("Unknown role value: " + value);
+//    }
+}
